64e79b9907f9f83a2de8cc82f3768edd466f3a28
Dadi Atar <atardadi@gmail.com>
2015-07-04 19:08:00 +0300

    yeoman generator

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name playalongWebApp
+ * @description
+ * # playalongWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('playalongWebApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name playalongWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the playalongWebApp
+ */
+angular.module('playalongWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
