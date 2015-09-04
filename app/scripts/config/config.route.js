f1a291967a8e142b8ffe31ff439c3e915fd5b478
Dadi Atar <atardadi@gmail.com>
2015-09-05 02:49:55 +0300

    chords top 10 page by hit count

--- app/scripts/config/config.route.js
+++ app/scripts/config/config.route.js
@@ -3,13 +3,17 @@
 app.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
       $stateProvider
-        .state("home",          { url: "/",               templateUrl: '../../views/home.html',     controller: 'HomeCtrl' })
+        .state("home",{ url: "/",               templateUrl: '../../views/home.html',     controller: 'HomeCtrl' })
         .state("chord",         { url: "/chord/",      templateUrl: '../../views/chord.html',    controller: 'ChordCtrl' })
+        .state('topChords',  { 
+        		url: '/topChords',
+        		templateUrl: '../../views/topChords.html',
+        		controller: 'TopchordsCtrl'
+      	})
 
         .state('builder',       { url: '/builder',        template: '<div ui-view></div>',          controller: 'BuilderCtrl', abstract: true})
         .state('builder.new',   { url: '/new',            templateUrl: '../../views/builder.html'})
-        .state('builder.edit',  { url: '/edit/:id',       templateUrl: '../../views/builder.html'})
-      ;
+        .state('builder.edit',  { url: '/edit/:id',       templateUrl: '../../views/builder.html'});
 
       $urlRouterProvider.otherwise('/builder/new');
 }]);
