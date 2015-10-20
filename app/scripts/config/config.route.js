8f15061b3d8e03c3fa70cc8c8f76b667ffafca65
Dadi Atar <atardadi@gmail.com>
2015-10-21 00:43:40 +0300

    need login on chord builder

--- app/scripts/config/config.route.js
+++ app/scripts/config/config.route.js
@@ -22,6 +22,14 @@ app.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $ur
         .state('builder',       { url: '/builder',        template: '<div ui-view></div>', abstract: true})
         .state('builder.edit', { 
           url: '/edit/:id',
+          resolve: {
+            // controller will not be loaded until $waitForAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function(Auth) {
+              // $waitForAuth returns a promise so the resolve waits for it to complete
+              return Auth.$waitForAuth();
+            }]
+          },
           templateUrl: '../../views/builder.html',
           controller: 'BuilderCtrl'
         })
---
+++ app/scripts/config/config.run.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.run(["$rootScope", "$state", function($rootScope, $state) {
+	$rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+	  // We can catch the error thrown when the $requireAuth promise is rejected
+	  // and redirect the user back to the home page
+	  if (error === "AUTH_REQUIRED") {
+	    $state.go("home");
+	  }
+	});
+}]);
