5323112a1a569f2f5232be3308053a8fe3c620fa
Dadi Atar <atardadi@gmail.com>
2015-11-11 01:41:58 +0200

    translate  - fixed buider page

--- app/scripts/config/config.constants.js
+++ app/scripts/config/config.constants.js
@@ -1,6 +1,7 @@
 app.constant('paths', {
 	images: {
 		logo: 'images/plyIcon.png',
-		emptyAvatar: './images/avatar.png'
+		emptyAvatar: './images/avatar.png',
+		flags: './images/flags.png'
 	}
 });
--- app/scripts/directives/languagepicker.js
+++ app/scripts/directives/languagepicker.js
@@ -13,16 +13,9 @@ angular.module('playalongWebApp')
       restrict: 'E',
       link: function postLink(scope) {
     		var availableLocales = ['en','he'];
-    		scope.languages = [
-    			{
-    				locale: 'he',
-    				title: 'Hebrew'
-    			}
-    		];
-    		
 
     		var loc = $translate.proposedLanguage();
-    		scope.selectedLanguage = loc === 'he' ? scope.languages[0] : scope.languages[1];
+    		scope.selectedLanguage = loc === 'he' ? 'hebrew' : 'english';
     	  scope.changeLocale = function(locale) {
     	  	if (locale && availableLocales.indexOf(locale) !== -1)
     	  	{
