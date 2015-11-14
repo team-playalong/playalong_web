5186abe68faa6891e45a02d40f7bef10eb821a62
Dadi Atar <atardadi@gmail.com>
2015-11-14 23:30:56 +0200

    revert disqus

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,28 +20,6 @@ var app = angular.module('playalongWebApp', [
   'textAngular',
   'ngDragDrop',
   'frapontillo.gage',
-  'pascalprecht.translate'
+  'pascalprecht.translate',
+  'angularUtils.directives.dirDisqus'
 ]);
-
-//Allow unafe html binding
-//TODO - refactor into whitelist
-app.config(function($sceProvider) {
-  // Completely disable SCE.  For demonstration purposes only!
-  // Do not use in new projects.
-  $sceProvider.enabled(false);
-});
-
-app.run(
-  [          '$rootScope', '$state', '$stateParams',
-    function ($rootScope,   $state,   $stateParams) {
-
-    // It's very handy to add references to $state and $stateParams to the $rootScope
-    // so that you can access them from any scope within your applications.For example,
-    // <li ng-class="{ active: $state.include s('contacts.list') }"> will set the <li>
-    // to active whenever 'contacts.list' or one of its decendents is active.
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-    }
-  ]
-);
-
--- app/scripts/config/config.run.js
+++ app/scripts/config/config.run.js
@@ -68,3 +68,25 @@ app.run(["$rootScope", "$state" ,'$window', function($rootScope, $state,$window)
 	});
 }]);
 
+//Allow unafe html binding
+//TODO - refactor into whitelist
+app.config(function($sceProvider) {
+  // Completely disable SCE.  For demonstration purposes only!
+  // Do not use in new projects.
+  $sceProvider.enabled(false);
+});
+
+app.run(
+  [          '$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
+
+    // It's very handy to add references to $state and $stateParams to the $rootScope
+    // so that you can access them from any scope within your applications.For example,
+    // <li ng-class="{ active: $state.include s('contacts.list') }"> will set the <li>
+    // to active whenever 'contacts.list' or one of its decendents is active.
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    }
+  ]
+);
+
