5457e4c628bfd1c979be55811901878f29a0da1f
Dadi Atar <atardadi@gmail.com>
2016-02-03 09:14:56 +0200

    favorite label and youtube mobile fix

--- app/components/ply-youtube/ply-youtube.module.js
+++ app/components/ply-youtube/ply-youtube.module.js
@@ -1,5 +1,7 @@
-angular.module('plyYoutube', [])
-.directive('plyYoutube', [function () {
+angular.module('plyYoutube', [
+	'playalong.services'
+])
+.directive('plyYoutube', ['RegexStore',function (RegexStore) {
 	return {
 		restrict: 'E',
 		scope: {
@@ -7,9 +9,14 @@ angular.module('plyYoutube', [])
 			plyYoutubeHeight: '=',
 			plyYoutubeSrc: '='
 		},
-		template: '<iframe width="{{plyYoutubeWidth || 300}}" height="{{plyYoutubeHeight || 150}}" src="{{formatterUrl}}" frameborder="0" allowfullscreen></iframe>',
+		template: '<iframe id="plyYoutubeIframe" width="{{plyYoutubeWidth || 300}}" height="{{plyYoutubeHeight || 150}}" src="{{formatterUrl}}" frameborder="0" allowfullscreen></iframe>',
 		link: function (scope, iElement, iAttrs) {
 			scope.formatterUrl = 'https://www.youtube.com/embed/' + scope.plyYoutubeSrc;
+      var mobileRegex = RegexStore.get('mobile');
+      var iframe = iElement.find('#plyYoutubeIframe');
+      if(mobileRegex.test(navigator.userAgent)) {
+      	iframe.css('width','100%');
+      }
 		}
 	};
 }])
