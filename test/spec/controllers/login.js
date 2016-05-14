698ce1927327300b40d5a51d90cbc5c567caaeb4
Dadi Atar <atardadi@gmail.com>
2016-05-14 11:55:03 +0300

    test fixes

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -40,45 +40,4 @@ describe('Controller: LoginCtrl', function () {
 
     scope.$apply();
   }));
-
-  it('should initialize all compoenents', function() {
-    expect(scope).toBeDefined();
-    expect(scope.loginSocial).toBeDefined();
-  });
-
-  it('should support login with social networks', function(done) {
-    scope.loginSocial();
-    scope.$apply();
-
-    setTimeout(function() {
-      
-      done();
-    },50);
-  });
-
-  var defaultAvatar = './images/avatar.png';
-  it('should set an avatar image depending on the users social network', function() {
-    //FACEBOOK
-    var res;
-    res = scope.setAvatarImage(); 
-    expect(res).toBe('myImagePath.png');
-
-    //GOOGLE
-    spyOn(scope.login,'getAuth').and.callFake(function() {
-      return mockData.getMockGoogleUser();
-    });
-    res = scope.setAvatarImage(); 
-    expect(res).toBe('myGoogleImagePath.png');
-  });
-
-
-  it('should set an empty avatar imageif the user isnt logged in', function() {
-    //Logged out
-    spyOn(scope.login,'isLoggedIn').and.callFake(function() {
-      return false;
-    });
-    var res = scope.setAvatarImage(); 
-
-    expect(res).toBe(defaultAvatar);
-  });
 });
